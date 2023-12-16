ad77a55583345a02d7790384dfe1594ec0cd8231
0xGrizzly <gmcguir2@students.kennesaw.edu>
2023-12-16 14:25:39 -0500

    added toggleTodoStatus function

--- main.go
+++ main.go
@@ -36,6 +36,21 @@ func addTodo(context *gin.Context){
   context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
+func toggleTodoStatus(context *gin.Context) {
+   id := context.Param("id")
+  todo, err := getTodoByID(id)
+  
+  if err != nil {
+    context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+    return
+  }
+
+  todo.Completed = !todo.Completed
+
+  context.IndentedJSON(http.StatusOK, todo)
+
+}
+
 func getTodoByID(id string)(*todo, error) {
   for i, t := range todos{
     if t.ID == id{
@@ -62,6 +77,7 @@ func main () {
   router := gin.Default()
   router.GET("/todos", getTodos)
   router.GET("/todos/:id", getTodo)
+  router.PATCH("/todos/:id", toggleTodoStatus)
   router.POST("/todos", addTodo)
   router.Run("localhost:9090")
 }
